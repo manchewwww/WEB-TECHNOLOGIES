b7b2df18211666616b86e0fc5476cc8e14cb218b
manchewwww <georgimanchev2003@gmail.com>
2025-03-05 11:42:45 +0200

    Add function when click on li to delete it

--- 01-first_ex/index.js
+++ 01-first_ex/index.js
@@ -2,7 +2,10 @@ function addToDoList() {
     const input = document.querySelector("input");
     const ul = document.querySelector("ul");
     const li = document.createElement("li");
+
     li.textContent = input.value;
+    li.onclick = () => ul.removeChild(li);
+
     ul.appendChild(li);
     input.value = '';
 }

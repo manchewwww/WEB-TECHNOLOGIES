ddb250ca5a11ef8611abcd4ad4685b160f07a1c5
manchewwww <georgimanchev2003@gmail.com>
2025-04-15 19:50:50 +0300

    Implement TicketService with CRUD operations for ticket management

---
+++ Issue Tracker/backend/services/TicketService.ts
@@ -0,0 +1,23 @@
+const ticketRepository = require('../repositories/TicketRepository');
+
+export const TicketService = {
+  async getAllTickets() {
+    return await ticketRepository.getAllTickets();
+  },
+
+  async getTicketById(id: number): Promise<any> {
+    return await ticketRepository.getTicketById(id);
+  },
+
+  async createTicket(ticketData: any): Promise<any> {
+    return await ticketRepository.createTicket(ticketData);
+  },
+
+  async editTicket(id: number, ticketData: any): Promise<any> {
+    return await ticketRepository.editTicket(id, ticketData);
+  },
+
+  async deleteTicket(id: number): Promise<boolean> {
+    return await ticketRepository.deleteTicket(id);
+  },
+};

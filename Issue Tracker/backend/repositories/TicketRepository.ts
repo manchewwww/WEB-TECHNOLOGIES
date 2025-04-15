f2f10529ea609c8a18dc093a520bd170db6ae0b6
manchewwww <georgimanchev2003@gmail.com>
2025-04-15 19:50:31 +0300

    Refactor TicketRepository to use specific types for tickets and ticket data

--- Issue Tracker/backend/repositories/TicketRepository.ts
+++ Issue Tracker/backend/repositories/TicketRepository.ts
@@ -1,50 +1,42 @@
-//TODO: Add class for ticketData
-let tickets: any[] = [];
+import { Ticket, TicketData } from '../models/Ticket';
+
+let tickets: Ticket[] = [];
 let id: number = 1;
 
-//TODO: Add class for return type
-function getAllTickets(): any[] {
-    return tickets;
-}
-//TODO: Add class for return type
-function getTicketById(ticketId: number): any {
-    const ticket = tickets.find(ticket => ticket.id === ticketId);
-    if (!ticket) {
-        throw new NotFoundError(`Ticket with ID ${ticketId} not found`);
+export const TicketRepository = {
+    getAllTickets(): Ticket[] {
+        return tickets;
+    },
+
+    getTicketById(ticketId: number): Ticket {
+        const ticket = tickets.find(ticket => ticket.id === ticketId);
+        if (!ticket) {
+            throw new NotFoundError(`Ticket with ID ${ticketId} not found`);
+        }
+        return ticket;
+    },
+
+    createTicket(ticketData: TicketData): Ticket {
+        const newTicket: Ticket = { id: id++, ...ticketData };
+        tickets.push(newTicket);
+        return newTicket;
+    },
+
+    editTicket(ticketId: number, ticketWithNewData: TicketData): Ticket {
+        const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
+        if (ticketIndex === -1) {
+            throw new NotFoundError(`Ticket with ID ${ticketId} not found`);
+        }
+        tickets[ticketIndex] = { ...tickets[ticketIndex], ...ticketWithNewData };
+        return tickets[ticketIndex];
+    },
+
+    deleteTicket(ticketId: number): boolean {
+        const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
+        if (ticketIndex === -1) {
+            throw new NotFoundError(`Ticket with ID ${ticketId} not found`);
+        }
+        tickets.splice(ticketIndex, 1);
+        return true;
     }
-    return ticket;
-}
-
-//TODO: Add class for ticketData and for return type
-function createTicket(ticketData: any): any {
-    tickets.push({ id: id++, ...ticketData });
-    return tickets[tickets.length - 1];
-}
-
-//TODO: Add class for ticketData and return type
-function editTicket(ticketId: number, ticketWithNewData: any): any {
-    const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
-    if (ticketIndex === -1) {
-        throw new NotFoundError(`Ticket with ID ${ticketId} not found`);
-    }
-    tickets[ticketIndex] = { ...tickets[ticketIndex], ...ticketWithNewData };
-    return tickets[ticketIndex];
-}
-
-function deleteTicket(ticketId: number): boolean {
-    const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
-    if (ticketIndex === -1) {
-        throw new NotFoundError(`Ticket with ID ${ticketId} not found`);
-    }
-    tickets.splice(ticketIndex, 1);
-    return true;
-}
-
-module.exports = {
-    getAllTickets,
-    getTicketById,
-    createTicket,
-    editTicket,
-    deleteTicket,
 };
-

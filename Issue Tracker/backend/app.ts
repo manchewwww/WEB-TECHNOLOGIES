3a3d33640d2600b14687e5c390e40bc8aef18034
manchewwww <georgimanchev2003@gmail.com>
2025-04-24 23:45:55 +0300

    Add Swagger UI integration and improve server logging

--- Issue Tracker/backend/app.ts
+++ Issue Tracker/backend/app.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swagger/swagger';
+import cors from 'cors';
 import tickersRouter from './controllers/TicketController';
 
+
 const app = express();
-const PORT = 3000;
+app.use(cors());
 
 app.use(express.json());
-app.use('/tickets',tickersRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/tickets', tickersRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+    console.log('Swagger docs at http://localhost:3000/api-docs');
 });

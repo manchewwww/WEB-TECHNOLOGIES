cb59d1962e35205c0f683d3d278f1684ef79228d
manchewwww <georgimanchev2003@gmail.com>
2025-04-24 23:48:24 +0300

    Refactor TicketService to remove unnecessary await statements for improved readability

--- Issue Tracker/backend/services/TicketService.ts
+++ Issue Tracker/backend/services/TicketService.ts
@@ -4,23 +4,23 @@ import ticketRepository from "../repositories/TicketRepository";
 
 const TicketService = {
   async getAllTickets(): Promise<Ticket[]>{
-    return await ticketRepository.getAllTickets();
+    return ticketRepository.getAllTickets();
   },
 
   async getTicketById(id: number): Promise<Ticket> {
-    return await ticketRepository.getTicketById(id);
+    return ticketRepository.getTicketById(id);
   },
 
   async createTicket(ticketData: TicketData): Promise<Ticket> {
-    return await ticketRepository.createTicket(ticketData);
+    return ticketRepository.createTicket(ticketData);
   },
 
   async editTicket(id: number, ticketData: any): Promise<Ticket> {
-    return await ticketRepository.editTicket(id, ticketData);
+    return ticketRepository.editTicket(id, ticketData);
   },
 
   async deleteTicket(id: number): Promise<boolean> {
-    return await ticketRepository.deleteTicket(id);
+    return ticketRepository.deleteTicket(id);
   },
 };
 

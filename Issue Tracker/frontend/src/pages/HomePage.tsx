ca430d3faa97ed1462c566b9023bca819efdd966
Radoslav Angelov <106675651+Radi03825@users.noreply.github.com>
2025-04-27 15:15:48 +0300

    Аdd main pages: HomePage, LoginPage, RegisterPage
    * Set up initial frontend project structure
    
    * Add Home, Login and Register pages
    
    * Implement authentication context and login functionality with LocalStorage

---
+++ Issue Tracker/frontend/src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+import '../styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <main className="homepage-content">
+        <div className="description-box">
+          <h2>Добре дошли в нашия Issue Tracker!</h2>
+          <p>
+            Това приложение ви позволява да създавате, проследявате и управлявате билети (issues)
+            по проекти. Създадено с Node.js, React, Typescript и MongoDB.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;

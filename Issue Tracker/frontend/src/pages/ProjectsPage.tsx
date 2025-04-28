b1be54b2b845ef75b068225b28d4a494058fcde3
Radoslav Angelov <106675651+Radi03825@users.noreply.github.com>
2025-04-28 20:49:42 +0300

    Add Admin and Projects pages, and styles for Home, Login, Register, and NavBar

---
+++ Issue Tracker/frontend/src/pages/ProjectsPage.tsx
@@ -0,0 +1,10 @@
+function ProjectsPage() {
+
+  return (
+    <div className="projects-page">
+      <h1 className="projects-title">Projects</h1>
+    </div>
+  );
+}
+
+export default ProjectsPage;

18dca4137c7b58b4d64916d8ae7935cc92e6f6fe
IvoJoy <ivkata1112@abv.bg>
2025-04-29 00:29:53 +0300

    resolving conflict

--- Issue Tracker/frontend/src/App.tsx
+++ Issue Tracker/frontend/src/App.tsx
@@ -4,12 +4,9 @@ import NavBar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-<<<<<<< HEAD
 import TicketPage from './pages/TicketPage';
-=======
 import ProjectsPage from './pages/ProjectsPage';
 import AdminPage from './pages/AdminPage';
->>>>>>> develop
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -21,12 +18,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-<<<<<<< HEAD
           <Route path="/tickets/:id" element={<TicketPage />} />
-=======
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/admin" element={<AdminPage />} />
->>>>>>> develop
         </Routes>
       </Router>
     </AuthProvider>

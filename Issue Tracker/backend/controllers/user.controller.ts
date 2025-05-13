ed9ba0a4185b4aa1254b7734a40a034f3d14ea02
manchewwww <georgimanchev2003@gmail.com>
2025-05-13 19:51:20 +0300

    Fix variable naming in getAllUsers method for consistency and remove unnecessary console log

--- Issue Tracker/backend/controllers/user.controller.ts
+++ Issue Tracker/backend/controllers/user.controller.ts
@@ -15,8 +15,8 @@ class UserController {
 
     private async getAllUsers(req: Request, res: Response): Promise<void> {
         try {
-            const user = await userService.getAllUsers();
-            res.status(201).json(user);
+            const users = await userService.getAllUsers();
+            res.status(201).json(users);
         } catch (error: any) {
             res.status(400).json({ message: error.message });
         }
--- Issue Tracker/backend/services/user.service.ts
+++ Issue Tracker/backend/services/user.service.ts
@@ -3,7 +3,6 @@ import UserRepository from "../repositories/user.repository";
 class UserService {
     async getAllUsers() {
         const users = await UserRepository.getAllUsers();
-        console.log(users);
         return users.map(user => {
             return { id: user._id, username: user.username }
         })
